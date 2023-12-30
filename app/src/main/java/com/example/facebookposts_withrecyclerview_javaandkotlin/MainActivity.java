b32d5f33f3210c442248438f22c85161ceb88224
mahhmoud refaat <mahhmoudrefat@outlook.com>
2023-12-30 08:46:48 +0200

    small content changes

--- app/src/main/java/com/example/facebookposts_withrecyclerview_javaandkotlin/MainActivity.java
+++ app/src/main/java/com/example/facebookposts_withrecyclerview_javaandkotlin/MainActivity.java
@@ -21,9 +21,11 @@ public class MainActivity extends AppCompatActivity {
 
         postList = new ArrayList<>(4);
         postList.add(new PostData("Mahmoud Refaat",3,"I get about 80 a week right now. I can not see why! This is being caused from loads of all ","http://unblast.com/color-schemes",R.drawable.imagepost3,20,15));
-        postList.add(new PostData("Moahmed Refaat",6,"Buckle up , because change is coming to WordPress","http://unblast.com/color-schemes",R.drawable.post_photo,20,15));
+        postList.add(new PostData("Moahmed Refaat",6,"Buckle up , because change is coming to WordPress","http://unblast.com/color-schemes",R.drawable.post_photo,6,5));
         postList.add(new PostData("Abdelrahman Refaat",10,"Ah dang I do have some resources in /drawable-mdpi and others just in /drawable. I've been running this on a 1.6 emulator for some time though","http://unblast.com/color-schemes",R.drawable.imagepost3,30,8));
         postList.add(new PostData("ahmed Refaat",20,"When you build the application R.java contains all the memory address and it happens that those address are invalid for next run","http://unblast.com/color-schemes",R.drawable.post_photo,70,9));
+        postList.add(new PostData("hussin ahmed",20,"When you build the application R.java contains all the memory address and it happens that those address are invalid for next run","http://unblast.com/color-schemes",R.drawable.post_photo,70,9));
+        postList.add(new PostData("yoyo Refaat",20,"When you build the application R.java contains all the memory address and it happens that those address are invalid for next run","http://unblast.com/color-schemes",R.drawable.post_photo,70,9));
 
         adapter = new postsRecyclerAdapter(postList);
         PostsRecycler.setAdapter(adapter);
