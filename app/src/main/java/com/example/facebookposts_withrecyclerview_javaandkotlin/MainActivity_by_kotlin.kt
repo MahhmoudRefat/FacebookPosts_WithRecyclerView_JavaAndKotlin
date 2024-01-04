88d11f656f3b161926024c5aef69cafd7f05cc05
mahhmoud refaat <mahhmoudrefat@outlook.com>
2024-01-04 15:50:21 +0200

    content edited

--- app/src/main/java/com/example/facebookposts_withrecyclerview_javaandkotlin/MainActivity_by_kotlin.kt
+++ app/src/main/java/com/example/facebookposts_withrecyclerview_javaandkotlin/MainActivity_by_kotlin.kt
@@ -27,6 +27,8 @@ class MainActivity_by_kotlin : AppCompatActivity() {
             ContactList.add(PostData_Kt("Mahmoud Refaat",4,"I get about 80 a week right now. I can not see why! This is being caused from loads of all ","http://unblast.com/color-schemes",5,10,R.drawable.post_photo));
             ContactList.add(PostData_Kt("ahmed Refaat",4,"Buckle up , because change is coming to WordPress ","http://unblast.com/color-schemes",30,25,R.drawable.imagepost3));
             ContactList.add(PostData_Kt("mohamed Refaat",4,"When you build the application R.java contains all the memory address and it happens that those address are invalid for next run","http://unblast.com/color-schemes",5,10,R.drawable.post_photo));
+            ContactList.add(PostData_Kt("mohamed Refaat",4,"When you build the application R.java contains all the memory address and it happens that those address are invalid for next run","http://unblast.com/color-schemes",5,10,R.drawable.post_photo));
+            ContactList.add(PostData_Kt("mohamed Refaat",4,"When you build the application R.java contains all the memory address and it happens that those address are invalid for next run","http://unblast.com/color-schemes",5,10,R.drawable.post_photo));
 
         }
     }
